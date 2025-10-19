7461d8f1f748d1973ba4e85799c0f7bd0140c5e3
NitanSharma <sharmanitan938@gmail.com>
2025-10-19 09:54:27 +0530

    About Section Done

---
+++ portfolio/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import AboutSection from "@/components/AboutSection";
+
+
+export default function AboutPage() {
+  return (
+    <main className="bg-[#111111] min-h-screen w-full">
+      <AboutSection />
+    </main>
+  );
+}
