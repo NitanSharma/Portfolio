d0f523b18e5e84bece513c18b4e7d6be4c963281
NitanSharma <sharmanitan938@gmail.com>
2025-10-19 10:02:15 +0530

    Contact Page Done

---
+++ portfolio/src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import ContactSection from "@/components/ContactSection";
+
+export default function ContactPage() {
+  return (
+    <main className="bg-[#111111] min-h-screen w-full">
+      <ContactSection/>
+    </main>
+  );
+}
---
+++ portfolio/src/components/ContactSection.tsx
@@ -0,0 +1,30 @@
+import { FaEnvelope } from 'react-icons/fa';
+
+export default function ContactSection() {
+  return (
+    <section className="flex flex-col items-center justify-center text-center text-white min-h-screen w-full px-6 gap-6">
+      <h1 className="font-bold text-6xl mb-4">
+        Contact<span className="text-[#0AFF9D]">.</span>
+      </h1>
+      <h4 className="text-base sm:text-lg md:text-xl mb-6">
+        Feel free to reach out via email if you&apos;d like to connect!
+        <br />
+        Also find me on{' '}
+        <a
+          href="https://www.linkedin.com/in/nitin-sharma-cse/"
+          className="text-[#0AFF9D]"
+        >
+          LinkedIn
+        </a>{' '}
+        
+        , if that&apos;s more your speed.
+      </h4>
+      <a
+        href="mailto:sharmanitan938@gmail.com"
+        className="flex items-center gap-2 text-[#0AFF9D] text-lg sm:text-2xl md:text-2xl underline hover:text-white transition"
+      >
+        <FaEnvelope /> sharmanitan938@gmail.com
+      </a>
+    </section>
+  );
+}
